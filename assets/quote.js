837a7c041eb2e97310503d360fcf4f32aecaa626
Mert Bulan <mertbulan@gmail.com>
2020-01-04 17:54:52 +0100

    fix typo and add a new quote

--- assets/quote.js
+++ assets/quote.js
@@ -21,7 +21,8 @@ var quotes = ["Mezardaki en zengin adam olmak değil, yatağa yattığımda hari
               "Neden varız bilemiyorum ama eminim ki keyfimize bakmak için değil. - Ludwig Wittgenstein"];
 
 
-var quotesInEnglish = ["Very little is needed to make a happy life; it is all within yourself, in your way of thinking - Marcus Aurelius"]
+var quotesInEnglish = ["Very little is needed to make a happy life; it is all within yourself, in your way of thinking - Marcus Aurelius",
+                        "The happiness of your life depends upon the quality of your thoughts. - Marcus Aurelius"]
 
 function newQuote() {
   var randomNumber = Math.floor(Math.random() * (quotesInEnglish.length));
