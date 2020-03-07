e84ad9ca476adf2a08214391bd5287e1d4fb2bf9
Mert Bulan <mertbulan@gmail.com>
2020-03-07 15:47:13 +0100

    add a new quote

--- assets/quote.js
+++ assets/quote.js
@@ -22,7 +22,8 @@ var quotes = ["Mezardaki en zengin adam olmak değil, yatağa yattığımda hari
 
 
 var quotesInEnglish = ["Very little is needed to make a happy life; it is all within yourself, in your way of thinking - Marcus Aurelius",
-                        "The happiness of your life depends upon the quality of your thoughts. - Marcus Aurelius"]
+                        "The happiness of your life depends upon the quality of your thoughts. - Marcus Aurelius",
+                        "We suffer more often in imagination than in reality. - Seneca"]
 
 function newQuote() {
   var randomNumber = Math.floor(Math.random() * (quotesInEnglish.length));
