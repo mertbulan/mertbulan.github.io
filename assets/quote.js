9f41c93df14f0423b6b80afa8d63920a0e98a2dd
Mert Bulan <mertbulan@gmail.com>
2020-09-15 18:22:52 +0200

    some optimizations

--- assets/quote.js
+++
@@ -1,9 +0,0 @@
-var quotes = ["Very little is needed to make a happy life; it is all within yourself, in your way of thinking - Marcus Aurelius",
-              "The happiness of your life depends upon the quality of your thoughts. - Marcus Aurelius",
-              "We suffer more often in imagination than in reality. - Seneca"]
-
-function newQuote() {
-  var randomNumber = Math.floor(Math.random() * (quotes.length));
-  document.getElementById('quote').innerHTML = quotes[randomNumber];
-}
-newQuote();
